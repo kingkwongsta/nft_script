631eb60683423df0af22752d2f6616f965fc232b
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-09 23:21:15 -0700

    debugging for importing to mongodb

--- mongo_import.py
+++ mongo_import.py
@@ -6,8 +6,11 @@ os.chdir('./data_export')
 
 for filename in os.listdir("."):
     if filename.endswith(".json"):
+        print(f'****** {filename} ******')
         try:
-            os.system(f'mongoimport --uri {os.getenv("MONGO_URI")} --collection nft_test --type json --file {filename}')
+            # os.system(f'mongoimport --uri {os.getenv("MONGO_URI")} --collection nft_test --type json --file {filename}')
+            os.system(f'mongoimport --uri {os.getenv("MONGO_URI")} --collection nft_test --type json {filename}')
+            print(f'mongoimport --uri {os.getenv("MONGO_URI")} --collection nft_test --type json {filename}')
             print(f'successfully imported {filename}')
         except Exception as error:
             print(f'Error importing {filename}: {str(error)}')
