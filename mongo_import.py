2d09cc439cc94a73f5cdf4b613e3c6c0aafdb29b
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-09 22:54:55 -0700

    change to folder to run mongoimport commands

--- mongo_import.py
+++ mongo_import.py
@@ -2,4 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-print(os.getenv("NFTPORT_API"))
+for filename in os.listdir("./data_export"):
+    print(filename)
+    if filename.endswith(".json"):
+        os.chdir('./data_export')
+        os.system(f'mongoimport --uri {os.getenv("MONGO_URI")} --collection nft_test --type json --file {filename}')
