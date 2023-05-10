196ad8594666feb8b592d3fc5eb43e4b76234f3d
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-10 12:26:48 -0700

    set db and collection to variables for configuration

--- mongo_import.py
+++ mongo_import.py
@@ -2,14 +2,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+db = "test"
+collection = "test2"
+
 os.chdir('./data_export')
 
 for filename in os.listdir("."):
     if filename.endswith(".json"):
         print(f'****** {filename} ******')
         try:
-            # os.system(f'mongoimport --uri {os.getenv("MONGO_URI")} --collection nft_test --type json --file {filename}')
-            os.system(f'mongoimport --uri {os.getenv("MONGO_URI")} --collection test2 --type json "{filename}"')
+            os.system(f'mongoimport --uri {os.getenv("MONGO_URI")} --db {db} --collection {collection} --type json "{filename}"')
             print(f'successfully imported {filename}')
         except Exception as error:
             print(f'Error importing {filename}: {str(error)}')
