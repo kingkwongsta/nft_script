8637f63fdca6fbd9286c57c04f17cd8ebc237583
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-09 23:10:50 -0700

    try exception for running json file creation

--- dataexport.py
+++ dataexport.py
@@ -8,8 +8,9 @@ headers = {
     "accept": "application/json",
     "Authorization": os.getenv("NFTPORT_API")
 }
+numNFTS = 20
 
-topURL = url = "https://api.nftport.xyz/v0/contracts/top?page_size=1&page_number=1&period=24h&order_by=volume&chain=ethereum"
+topURL = url = f'https://api.nftport.xyz/v0/contracts/top?page_size={numNFTS}&page_number=1&period=24h&order_by=volume&chain=ethereum'
 
 
 response = requests.get(topURL, headers=headers)
@@ -18,8 +19,11 @@ addresses = data["contracts"]
 
 
 for item in addresses:
-    url = f'https://api.nftport.xyz/v0/nfts/{item["contract_address"]}?chain=ethereum&page_number=1&page_size=50&include=metadata&include=file_information&include=rarity&include=last_sale_price&include=all&refresh_metadata=false'
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    with open(f'data_export/{item["name"]}.json', 'w') as file:
-        json.dump(data, file)
+    try:
+        url = f'https://api.nftport.xyz/v0/nfts/{item["contract_address"]}?chain=ethereum&page_number=1&page_size=50&include=metadata&include=file_information&include=rarity&include=last_sale_price&include=all&refresh_metadata=false'
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        with open(f'data_export/{item["name"]}.json', 'w') as file:
+            json.dump(data, file)
+    except Exception as error:
+        print(f'Error creating file {item["name"]}.json: {str(e)}')
--- mongo_import.py
+++ mongo_import.py
@@ -2,8 +2,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-for filename in os.listdir("./data_export"):
+os.chdir('./data_export')
+
+for filename in os.listdir("."):
     print(filename)
     if filename.endswith(".json"):
-        os.chdir('./data_export')
         os.system(f'mongoimport --uri {os.getenv("MONGO_URI")} --collection nft_test --type json --file {filename}')
