d6f39b7615f2af6ee70e27474707614218308655
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-10 23:06:18 -0700

    move header outside of function and remove top_addresses to a separate function

--- dataexport.py
+++ dataexport.py
@@ -8,28 +8,44 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
 headers = {
-    "accept": "application/json",
-    "Authorization": os.getenv("NFTPORT_API")
-}
-numNFTS = 5
+        "accept": "application/json",
+        "Authorization": os.getenv("NFTPORT_API")
+    }
 
-topURL = url = f'https://api.nftport.xyz/v0/contracts/top?page_size={numNFTS}&page_number=1&period=24h&order_by=volume&chain=ethereum'
+def get_top_addresses():
+    topURL = f'https://api.nftport.xyz/v0/contracts/top?page_size={numNFTS}&page_number=1&period=24h&order_by=volume&chain=ethereum'
 
-##API CALL TO GET TOP X# of NFT ADDRESSESS
-response = requests.get(topURL, headers=headers)
-data = response.json()
-addresses = data["contracts"]
+    ##API CALL TO GET TOP X# of NFT ADDRESSESS
+    response = requests.get(topURL, headers=headers)
+    data = response.json()
+    addresses = data["contracts"]
+    return addresses
 
 
-for item in addresses:
-    try:
-        url = f'https://api.nftport.xyz/v0/nfts/{item["contract_address"]}?chain=ethereum&page_number=1&page_size=50&include=metadata&include=file_information&include=rarity&include=last_sale_price&include=all&refresh_metadata=false'
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        data["date_pulled"] = datetime.datetime.now().strftime("%Y-%m-%d")
-        with open(f'data_export/{datetime.datetime.now().strftime("%Y-%m-%d")}-{item["name"]}.json', 'w') as file:
-            json.dump(data, file)
-            print(f'File {datetime.datetime.now().strftime("%Y-%m-%d")}-{item["name"]}.json was successfully created.')
-    except Exception as error:
-        print(f'Error creating file {item["name"]}.json: {str(error)}')
+def download_top_nfts(numNFTS):
+    
+
+   
+
+    json_files = []
+
+    for item in addresses:
+        try:
+            url = f'https://api.nftport.xyz/v0/nfts/{item["contract_address"]}?chain=ethereum&page_number=1&page_size=50&include=metadata&include=file_information&include=rarity&include=last_sale_price&include=all&refresh_metadata=false'
+            response = requests.get(url, headers=headers)
+            data = response.json()
+            data["date_pulled"] = datetime.datetime.now().strftime("%Y-%m-%d")
+            filename = f'{datetime.datetime.now().strftime("%Y-%m-%d")}-{item["name"]}.json'
+            with open(f'data_export/{filename}', 'w') as file:
+                json.dump(data, file)
+                print(f'File {filename} was successfully created.')
+                json_files.append(filename)
+        except Exception as error:
+            print(f'Error creating file {item["name"]}.json: {str(error)}')
+
+    return json_files
+
+
+download_top_nfts(1)
