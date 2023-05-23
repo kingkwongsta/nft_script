7bc4b86a156ada08dfe5a20ac1b601b06c410f07
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-22 20:28:49 -0700

    clean up comments and code

---
+++ todo.py
@@ -0,0 +1,6 @@
+###### TODO #####
+
+## have mongo_import run after this script executes
+## convert to classes
+## after import to mongodb - set how many were successful and failed
+## try using mongodb data!
