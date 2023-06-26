a92dd8a9ba72dd10b483717e8ea49f097c281f4d
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-06-26 14:35:10 -0700

    add blockspan api call

--- blockspan_export.py
+++ blockspan_export.py
@@ -5,9 +5,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-HEADERS = {
+NFTPORT_HEADERS = {
         "accept": "application/json",
         "Authorization": os.getenv("NFTPORT_API")
+    }
+BLOCKSPAN_HEADERS = {
+        "accept": "application/json",
+        "X-API-KEY": os.getenv("BLOCKSPAN_API")
     }
 
 ##function to get the top number of NFTs
@@ -25,10 +29,12 @@ def download_top_nfts(numNFTS):
     
     addresses = get_top_addresses(numNFTS)
     json_files = []
+    
+
 
     for item in addresses:
         try:
-            url = f'https://api.nftport.xyz/v0/nfts/{item["contract_address"]}?chain=ethereum&page_number=1&page_size=50&include=metadata&include=file_information&include=rarity&include=last_sale_price&include=all&refresh_metadata=false'
+            url = f'https://api.blockspan.com/v1/nfts/contract/{item["contract_address"]}?chain=eth-main&include_current_owners=true&include_recent_price=true&page_size=50'
             response = requests.get(url, headers=HEADERS)
             data = response.json()
             data["date_pulled"] = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -41,5 +47,3 @@ def download_top_nfts(numNFTS):
             print(f'Error creating file {item["name"]}.json: {str(error)}')
 
     return json_files
-
-    download_top_nfts(5)
