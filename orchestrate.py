9712cffe4876ab7f9c533e29ee173f4c0cb536f8
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-06-26 19:07:53 -0700

    import/export sales data

--- orchestrate.py
+++ orchestrate.py
@@ -1,14 +1,14 @@
 ##create a summary of the number of files exported and imported
 
 from dataexport import get_top_addresses, download_top_nfts as download_top_nfts_dataexport
-from mongo_import import import_collection_to_mongodb
+from mongo_import import import_collection_to_mongodb, import_sales_to_mongodb
 from blockspan_export import download_top_nfts as download_top_nfts_blockspan
 from sales_data import download_sales_data
 
 # download_top_nfts_blockspan(5)
 
-download_top_nfts_dataexport(40)
-import_collection_to_mongodb("web3", "eth_top_nfts")
+# download_top_nfts_dataexport(40)
+# import_collection_to_mongodb("web3", "eth_top_nfts")
 
 download_sales_data(40)
-import_collection_to_mongodb("web3", "eth_top_sales")
+import_sales_to_mongodb("web3", "eth_top_sales")
